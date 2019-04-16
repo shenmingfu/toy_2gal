3b769af420be22accd58b0970dfae4c6faca9cf3
shenmingfu <31140462+shenmingfu@users.noreply.github.com>
2019-04-16 11:57:05 -0400

    Update generate.sh

--- toy_template/generate.sh
+++ toy_template/generate.sh
@@ -6,7 +6,5 @@ do
 
 /users/sfu1/Documents/jedisim5/jedinoise empty.fits 1 0.0077 noise_${i}.fits
 sleep 3s
-#/users/sfu1/Documents/jedisim5/jedinoise empty.fits 1 0.0077 noise_b${i}_90.fits
-#sleep 3s
 
 done
